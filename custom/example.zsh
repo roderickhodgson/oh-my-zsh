a19c51347e7f0ea62d3421e335e2c25f0ffb039c
Roderick Hodgson <roderick.hogson@googlemail.com>
2011-07-20 17:51:14 +0100

    Modified to work with peggy box

--- custom/example.zsh
+++ custom/example.zsh
@@ -4,12 +4,10 @@
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
 # Added by autojump install.sh
-source /etc/profile.d/autojump.zsh
+#source /etc/profile.d/autojump.zsh
 
 alias py=python
 alias ipy=ipython
-alias shake='cd /project/vp/home/roderick/trunk/src/support_packages/VPlibs/core && svn up && cd /project/vp/home/roderick/trunk/src/support_packages/VPlibs/python && svn up && cd /project/vp/home/roderick/trunk && svn up && cd /project/vp/home/roderick/trunk/src/support_packages/'
-alias bake="cd /project/vp/home/roderick/trunk/src/support_packages/ && make FORPYTHON=1 && make FORPYTHON=1 install && cd -"
 alias space="du -sk .[!.]* *| sort -n"
 # command L equivalent to command |less
 alias -g L='|less' 
@@ -17,6 +15,3 @@ alias -g L='|less'
 # command S equivalent to command &> /dev/null &
 alias -g S='&> /dev/null &'
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
-export PATH=$PATH:/project/vp/home/roderick/node/bin
