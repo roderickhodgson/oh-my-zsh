a8643b259966b3c7001ca2ec365496d60b0e83c7
Roderick Hodgson <roderick.hogson@googlemail.com>
2011-07-23 15:58:17 +0100

    Added plugin for my custom autojump installation

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,13 +3,9 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
-# Added by autojump install.sh
-source /etc/profile.d/autojump.zsh
 
 alias py=python
 alias ipy=ipython
-alias shake='cd /project/vp/home/roderick/trunk/src/support_packages/VPlibs/core && svn up && cd /project/vp/home/roderick/trunk/src/support_packages/VPlibs/python && svn up && cd /project/vp/home/roderick/trunk && svn up && cd /project/vp/home/roderick/trunk/src/support_packages/'
-alias bake="cd /project/vp/home/roderick/trunk/src/support_packages/ && make FORPYTHON=1 && make FORPYTHON=1 install && cd -"
 alias space="du -sk .[!.]* *| sort -n"
 # command L equivalent to command |less
 alias -g L='|less' 
@@ -17,6 +13,6 @@ alias -g L='|less'
 # command S equivalent to command &> /dev/null &
 alias -g S='&> /dev/null &'
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
-export PATH=$PATH:/project/vp/home/roderick/node/bin
+# MacPorts Installer addition on 2011-04-08_at_10:26:03: adding an appropriate PATH variable for use with MacPorts.
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+
